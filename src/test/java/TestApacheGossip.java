ec1d5e6b2f55c5459dde8d7132db21a3736f2186
TimVan <877020296@qq.com>
2020-03-27 20:46:52 +0800

    整合Apache Gossip

---
+++ src/test/java/TestApacheGossip.java
@@ -0,0 +1,12 @@
+/**
+ * <h3>BlockChain</h3>
+ * <p></p>
+ *
+ * @author : TimVan
+ * @date : 2020-03-27 18:27
+ **/
+public class TestApacheGossip {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
